edf962c0714e5143cca8ef86974b1ec92a537dfb
ianhussey <Ian@Ians-MacBook-Air.local>
2016-09-05 17:04:33 +0200

    BEST updates

--- BEST.R
+++ BEST.R
@@ -1,10 +1,27 @@
 ###################################################################
-# Automated reporting of BEST test (bayesian t test alternative)
-# for use in knittr scripts
+# Automated reporting of Bayesian Estimation Superceeds the Ttest 
+# (BEST: Krushke, 2013), a bayesian t test alternative.
+# see http://sumsar.net/blog/2014/02/bayesian-first-aid-two-sample-t-test/
 
 # Author: Ian Hussey (ian.hussey@ugent.be)
 
-# NB USES A NON INFORMATIVE PRIOR BY DEFAULT, SEE KRUSHKE 2013
+# Model to describe data
+#   means of both conditions (μ1, μ2), 
+#   SDs of both conditions (σ1, σ2), 
+#   shared normality parameter (ν).
+
+# Prior
+# Krushke (2013) decribes a specific broad/vague default prior, 
+# HOWEVER THE BEST PACKAGE USED HERE EMPLOYS A DIFFERENT DEFAULT PRIOR.
+# Kruschke (2016, personal communication) argues that both are equally broad and vague.
+# For each sample yi in (y1, y2), 
+#   μi = normal(M = mean(yi), SD = sd(yi)*5))
+#   σi = gamma(Mo = sd(yi), SD = sd(yi)*5)
+#   ν = gamma(M = 30, SD = 30)
+
+# Assumptions of script:
+# 1. Comparison value (compVal) = 0
+# 2. ROPE is placed on effect size (e.g., rather than mean difference)
 
 ###################################################################
 # Clean the workspace
@@ -21,11 +38,13 @@ setwd("~/git/Automated Reporting/")
 data_df <- read.csv("dataset.csv")
 
 ###################################################################
-# analyses
+# analysis
 
-# BEST
-# see http://sumsar.net/blog/2014/02/bayesian-first-aid-two-sample-t-test/
-## NB USES A NON-INFORMATIVE PRIOR BY DEFAULT
+# region of practical equivalence (ROPE) for assessing group equality.
+# placed on effect size (d). Selected for corrispondance with effects smaller than "small" by Cohen's guidelines (1988).
+ROPE <- c(-0.2, 0.2)
+
+# BEST test
 attach(data_df)
 BEST <- BESTmcmc(change_score[condition == "a"],
                          change_score[condition == "b"])
@@ -33,71 +52,102 @@ detach(data_df)
 
 # save analysis to disk 
 save(BEST, file="BEST.RData")
+
 # Load previously saved analysis from disk
 #load(file="BEST.RData")
 
-# define ROPE and tidy up output
+# tidy up output
 BEST_output_df <- 
-  as.data.frame(summary(BEST, 
-                        ROPEeff = c(-0.2,0.2))) %>%  # ROPE defined here
-  tibble::rownames_to_column() %>%  # convert rowname to column
-  mutate(mode = round(mode, 2),  # round values and rename awkwardly named ones
+  summary(BEST, ROPEeff = ROPE) %>%
+  as.data.frame() %>%  # convert to data frame for easier subsetting
+  tibble::rownames_to_column() %>%  # convert rowname to column for subsetting
+  mutate(mean = round(mean, 2),
+         median = round(median, 2),
+         mode = round(mode, 2),  # round values and rename awkwardly named ones
          HDIlo = round(HDIlo, 2),
          HDIup = round(HDIup, 2),
          percent_greater_than_zero = round(`%>compVal`, 2),
-         percent_in_rope = round(`%InROPE`, 2),
-         mean = round(mean, 2))
+         percent_in_rope = round(`%InROPE`, 2)) %>%
+  select(-`%InROPE`, -`%>compVal`)
 
 ########################################################################
-## extract individual stats
-  
-es_mode                   <- BEST_output_df %>% filter(rowname == "effSz") %>% .$mode  # convert df to individual variables
-es_hdi_low                <- BEST_output_df %>% filter(rowname == "effSz") %>% .$HDIlo  
-es_hdi_high               <- BEST_output_df %>% filter(rowname == "effSz") %>% .$HDIup  
-es_percent_greater_zero   <- BEST_output_df %>% filter(rowname == "effSz") %>% .$percent_greater_than_zero  
-es_percent_in_rope        <- BEST_output_df %>% filter(rowname == "effSz") %>% .$percent_in_rope  
-m_condition_a             <- BEST_output_df %>% filter(rowname == "mu1") %>% .$mean 
-m_condition_b             <- BEST_output_df %>% filter(rowname == "mu2") %>% .$mean 
+# View results
 
-########################################################################
-# convert output to natural langauge
-
-es_size <- ifelse(abs(es_mode) < 0.2, "negligable", 
-                  ifelse(abs(es_mode) < 0.5, "small", 
-                         ifelse(abs(es_mode) < 0.8, "medium", "large"))) 
-
-es_hid_includes_zero <- 
-  ifelse(m_condition_a < m_condition_b & m_condition_b < 0, "did not overlap zero",  # if (a,b) < 0
-         ifelse(m_condition_a > m_condition_b & m_condition_a > 0, "did not overlap zero", # if 0 < (a,b)
-                "overlapped zero"))  # if a < 0 < b
-
-conclusion <- 
-  ifelse(m_condition_a < m_condition_b & m_condition_b < 0, "credible differences",  # if (a,b) < 0
-         ifelse(m_condition_a > m_condition_b & m_condition_a > 0, "credible differences", # if 0 < (a,b)
-                "no credible differences"))  # if a < 0 < b
-
-BEST_text <- sprintf("A Bayesian BEST test was used to compare differences between the two conditions. We employed the default non-informative prior and parameters described by Krushke (2013). A region of practical equivalence (ROPE) was defined on the effect size (-0.2 < d < 0.2) in order to allow us to assess group equality as well as group differences. The posterior probabilities indicated that %s%% of credible effect sizes were greater than 0, and %s%% were within the ROPE. The most probable effect size was of %s size with a high density interval that %s, Mode d = %s, 95%% HDI [%s, %s]. We therefore concluded that %s existed between the low condition (M = %s) and high condition (M = %s).", 
-                     es_percent_greater_zero, 
-                     es_percent_in_rope, 
-                     es_size, 
-                     es_hid_includes_zero, 
-                     es_mode,
-                     es_hdi_low, 
-                     es_hdi_high, 
-                     conclusion, 
-                     m_condition_a, 
-                     m_condition_b)
-
-# write data to disk
-sink("output BEST.txt")
+# full output
 BEST_output_df
-cat("\n")
-cat("\n")
-cat(BEST_text)
-sink()
 
 # plot
-plotAll(BEST,  
-        ROPEeff=c(-0.2,0.2),
-        showCurve = TRUE)
+plotAll(BEST, ROPEeff=ROPE, showCurve = TRUE)
+
+########################################################################
+## extract individual variables for easier printing
+
+es_mode               <- BEST_output_df %>% filter(rowname == "effSz") %>% .$mode  
+es_hdi_low            <- BEST_output_df %>% filter(rowname == "effSz") %>% .$HDIlo  
+es_hdi_high           <- BEST_output_df %>% filter(rowname == "effSz") %>% .$HDIup  
+es_greater_zero       <- BEST_output_df %>% filter(rowname == "effSz") %>% .$percent_greater_than_zero  
+es_in_rope            <- BEST_output_df %>% filter(rowname == "effSz") %>% .$percent_in_rope  
+m_condition_a         <- BEST_output_df %>% filter(rowname == "mu1") %>% .$mean 
+m_condition_b         <- BEST_output_df %>% filter(rowname == "mu2") %>% .$mean 
+
+########################################################################
+# construct strings from output
+
+# set the DV and condition names 
+DV_name               <- "IAT D1 change scores"
+condition_a_name      <- "the flowers condition"
+condition_b_name      <- "the insects condition"
+
+# interpret effect size based on Cohen's (1988) guidelines
+es_size               <- ifelse(abs(es_mode) < 0.2, "negligable", 
+                                ifelse(abs(es_mode) < 0.5, "small", 
+                                       ifelse(abs(es_mode) < 0.8, "medium", "large"))) 
+
+# assess if >=95% of credible es are inside the ROPE
+equality_boolean      <- ifelse(es_in_rope >= 95, 1, 0)
+
+# assess if the 95% HDI includes the zero point
+differences_boolean   <- ifelse(m_condition_a < m_condition_b & m_condition_b < 0, 
+                                1,  # if (a,b) < 0
+                                ifelse(m_condition_a > m_condition_b & m_condition_a > 0, 
+                                       1, # if 0 < (a,b)
+                                       0))  # if a < 0 < b
+
+# set a string depending on the above boolean
+es_hid_includes_zero  <- ifelse(differences_boolean == 1, 
+                                "did not overlap zero", 
+                                "overlapped zero")
+
+# Assess 3 way decision path based on equality and differences booleans to make a final conclusion
+conclusions           <- ifelse(equality_boolean == 1,  # NB even if differences==1 here, effect is still so small as to consider groups equal.
+                                "were credibly equal. ",
+                                ifelse(differences_boolean == 1,
+                                       "were credbily different. ",
+                                       "were neither credibly different nor credibly equal. No firm conclusions could therefore be drawn"))
+
+########################################################################
+# combine all into a natural langauge string
+BEST_text             <- sprintf("A Bayesian BEST test (Kruschke, 2013) was used to compare differences in %s between %s and %s. The model employed 5 parameters to describe the data: the means of both conditions (μ1, μ2), the standard deviations of both conditions (σ1, σ2), and a shared normality parameter (ν). We employed the default BEST prior, which is a noncommittal prior intended to have minimal impact on the posterior distribution. Specifically, for sample yi in (y1, y2), μi = normal(M = mean(yi), SD = sd(yi)*5)), σi = gamma(Mo = sd(yi), SD = sd(yi)*5), ν = gamma(M = 30, SD = 30). In order to allow us to assess group equality as well as group differences, a region of practical equivalence (ROPE) was defined (negligible effect size: -0.2 < d < 0.2). The posterior probabilities indicated that %s%% of credible effect sizes were greater than 0, and %s%% were within the ROPE. The most credible effect size was of %s size with a highest density interval that %s, Mode d = %s, 95%% HDI [%s, %s]. We therefore concluded that %s between %s (M = %s) and %s (M = %s) %s", 
+                                 DV_name, condition_a_name, condition_b_name, es_greater_zero, es_in_rope, es_size, 
+                                 es_hid_includes_zero, es_mode, es_hdi_low, es_hdi_high, DV_name, condition_a_name, 
+                                 m_condition_a, condition_b_name, m_condition_b, conclusions)
+
+# returns: 
+# "A Bayesian BEST test (Kruschke, 2013) was used to compare differences in IAT D1 change scores between the flowers 
+# condition and the insects condition. The model employed 5 parameters to describe the data: the means of both conditions 
+# (μ1, μ2), the standard deviations of both conditions (σ1, σ2), and a shared normality parameter (ν). We employed the 
+# default BEST prior, which is a noncommittal prior intended to have minimal impact on the posterior distribution. 
+# Specifically, for sample yi in (y1, y2), μi = normal(M = mean(yi), SD = sd(yi)*5)), σi = gamma(Mo = sd(yi), 
+# SD = sd(yi)*5), ν = gamma(M = 30, SD = 30). In order to allow us to assess group equality as well as group differences, 
+# a region of practical equivalence (ROPE) was defined (negligible effect size: -0.2 < d < 0.2). The posterior probabilities 
+# indicated that 99.89% of credible effect sizes were greater than 0, and 1.53% were within the ROPE. The most credible 
+# effect size was of medium size with a highest density interval that did not overlap zero, Mode d = 0.67, 
+# 95% HDI [0.23, 1.14]. We therefore concluded that IAT D1 change scores between the flowers condition (M = 0.11) and the 
+# insects condition (M = -0.11) were credbily different. "
+
+########################################################################
+# write data to disk
+sink("output BEST.txt")
+cat(BEST_text)
+sink()
 
