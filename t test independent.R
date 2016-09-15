c2d23792d64ce1e9d238a969f0286bff55488832
ianhussey <Ian@g021h033.guest.ugent.be>
2016-09-15 17:08:49 +0200

    independent t test update
    more generic input

--- t test independent.R
+++ t test independent.R
@@ -3,16 +3,9 @@
 # for use in knittr scripts
 
 # Ian Hussey (ian.hussey@ugent.be)
+# see github.com/ianhussey/automatedreporting 
 
 # output checked against results returned by JASP
-
-# usage:
-# 1. customise the working directory line, containing setwd() below
-# 2. run script
-
-# to do:
-# report if one sided t test
-# 
 
 ########################################################################
 # Clean workspace
@@ -25,45 +18,43 @@ library(effsize)
 library(weights)  # for rd(), a round() alternative 
 
 ########################################################################
+# Specific data, variables, and parameters of test
+
+# labels
+DV_name                   <- "change scores"
+condition_a_name          <- "the flowers condition"
+condition_b_name          <- "the insects condition"
+condition_a_code_in_data  <- "a"
+condition_b_code_in_data  <- "b"
+output_file_name          <- "output independent t test.txt"
+
 # data acquisition
 setwd("~/git/Automated Reporting/")
 data_df <- read.csv("dataset.csv")
+attach(data_df)  # use the input data frame for all tests below
 
-########################################################################
-## tests
 # t test
-t_test <- t.test(formula = change_score ~ condition,  # IV ~ DV adjusted here
-                 data = data_df, 
+t_test <- t.test(formula = change_score ~ condition,    # IV ~ DV adjusted here
                  alternative = "two.sided",
                  paired = FALSE)
 
 # effect size
-cohens_d <- cohen.d(change_score ~ condition, 
-                    data = data_df,
+cohens_d <- cohen.d(change_score ~ condition,           # IV ~ DV adjusted here
                     paired = FALSE)
 
 # descriptive stats by cell
 desc_stats <- 
   ez::ezStats(data = data_df,
-              dv = .(change_score), 
+              dv = .(change_score),                     # DV adjusted here
               wid = .(participant),
-              between = .(condition),
+              between = .(condition),                  # IV adjusted here
               type = 3) %>%
-  mutate(Mean = round(Mean, 2),  # round for later reporting
+  mutate(Mean = round(Mean, 2),                        # round for later reporting
          SD = round(SD, 2)) %>%
   select(-FLSD)
 
 ########################################################################
 ## extract individual stats
-
-# ideally this would be left to schoRsch but D returns NA
-#my_t_test_output <- schoRsch::t_out(my_t_test, 
-#                                    n.equal = TRUE,
-#                                    welch.df.exact = TRUE, 
-#                                    welch.n = NA,
-#                                    d.corr = FALSE, 
-#                                    print = TRUE)
-#returns: d = NA for some reason. 
 
 # t test
 t_test_est        <- round(t_test$statistic[[1]], 2)
@@ -83,24 +74,19 @@ t_test_p <- ifelse(t_test_p < 0.001, "< .001",
                           paste("= ", rd(t_test_p, 2), sep = "")))
 
 ## descriptives
-a_m   <- desc_stats %>% filter(condition == "a") %>% .$Mean  # convert df to individual variables
-b_m   <- desc_stats %>% filter(condition == "b") %>% .$Mean
-a_sd  <- desc_stats %>% filter(condition == "a") %>% .$SD
-b_sd  <- desc_stats %>% filter(condition == "b") %>% .$SD
-a_n   <- desc_stats %>% filter(condition == "a") %>% .$N
-b_n   <- desc_stats %>% filter(condition == "b") %>% .$N
+a_m   <- desc_stats %>% filter(condition == condition_a_code_in_data) %>% .$Mean  # convert df to individual variables
+b_m   <- desc_stats %>% filter(condition == condition_b_code_in_data) %>% .$Mean
+a_sd  <- desc_stats %>% filter(condition == condition_a_code_in_data) %>% .$SD
+b_sd  <- desc_stats %>% filter(condition == condition_b_code_in_data) %>% .$SD
+a_n   <- desc_stats %>% filter(condition == condition_a_code_in_data) %>% .$N
+b_n   <- desc_stats %>% filter(condition == condition_b_code_in_data) %>% .$N
 
 ########################################################################
 # convert output to natural langauge
 
-# set the DV and condition names 
-DV_name               <- "IAT D1 change scores"
-condition_a_name      <- "the low condition"
-condition_b_name      <- "the high condition"
-
 nhst <- ifelse(t_test_p < 0.05, 
-               paste("An independent t test demonstrated significant differences of ", d_interpretation, " effect size in ", DV_name, " between ", sep = ""),
-               paste("An independent t test demonstrated non-significant differences of ", d_interpretation, " effect size between ", sep = ""))
+               paste("A Welch's independent t test demonstrated significant differences of ", d_interpretation, " effect size in ", DV_name, " between ", sep = ""),
+               paste("A Welch's independent t test demonstrated non-significant differences of ", d_interpretation, " effect size between ", sep = ""))
 
 # t test and d
 t_test_output <- paste(", t(", t_test_df, ") = ", t_test_est, ", p ", t_test_p, ", d = ", d_est, ", 95% CI [", d_ci_lower, ", ", d_ci_upper, "]. ", sep = "")
@@ -121,7 +107,7 @@ t_test_text <- paste(nhst,
                      sep = "")
 
 ## write to disk
-sink("output t test independent.txt")
+sink(output_file_name)
 cat(t_test_text)  # cat() supresses the line number from being printed
 sink()
 
